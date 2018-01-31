750bfa3753388f78b72fd6e4e1d7b3c2ce42a15e
Unknown <kerensahughes@gmail.com>
2018-01-31 12:08:31 -0500

    Initial Commit

---
+++ Lex's Path (1)/Assets/scripts/Character_behavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Character_behavior : MonoBehaviour {
+
+    float moveSpeed = 2f;
+    public CircleCollider2D charcollider;
+
+    Rigidbody2D charbody; 
+
+
+	// Use this for initialization
+	void Start () {
+        charbody = GetComponent<Rigidbody2D>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.GetComponent<Rigidbody2D>().AddForce(new Vector2(Input.GetAxis("Horizontal"), 0f), ForceMode2D.Impulse);
+
+        
+
+        if (this.GetComponent<Transform>().position.y < -5)
+        { //when the position on the x axis is greater than x=10
+
+            SceneManager.LoadScene("Game Over");
+        }
+    }
+}
+
+  
---
+++ Lex's Path (1)/Assets/scripts/Normal_Piece_Behavior.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Normal_Piece_Behavior : MonoBehaviour
+{
+
+
+    //  Vector2 point;  
+    // Use this for initialization
+    bool dragging = false;
+    float distance;
+
+    void OnMouseEnter()
+    {
+
+    }
+
+    void OnMouseExit()
+    {
+
+    }
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+    }
+
+    void OnMouseUp()
+    {
+        dragging = false;
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector2 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint;
+        }
+
+    }
+   /* void OnCollisionEnter2D (Collision2D collision)
+    {
+
+
+        if (collision.gameObject.tag.Equals("Wall"))
+        {
+
+            Destroy(this.gameObject);
+        }
+        */
+        /*
+        void OnMouseDrag()
+        {
+            point.y = gameObject.transform.position.y;
+
+
+            Debug.Log("Drag");
+
+
+        }
+        */
+
+
+
+    }
+
